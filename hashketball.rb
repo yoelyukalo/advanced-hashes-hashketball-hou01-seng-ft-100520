59c1635917b353abc4fdacc7e33735d0c72069a3
Yoel Yukalo <yoelyukalo@gmail.com>
2020-09-28 17:45:03 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -206,7 +206,7 @@ def player_stats(input)
       if key == :players
         value.each do |player|
           if input == player[:player_name]
-            player.delete(:player_name) 
+            # player.delete(:player_name) 
             return player
           end
         end
